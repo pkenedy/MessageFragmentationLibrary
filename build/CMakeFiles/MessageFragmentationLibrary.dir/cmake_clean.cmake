f8dec756c0c381304d88155c4387b2e376e8e0ad
Paul Kenedy <pkenedy48@gmail.com>
2024-10-30 01:30:59 +0300

    pages updates

---
+++ build/CMakeFiles/MessageFragmentationLibrary.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MessageFragmentationLibrary.dir/MessageFragmenter.cpp.o"
+  "CMakeFiles/MessageFragmentationLibrary.dir/MessageFragmenter.cpp.o.d"
+  "CMakeFiles/MessageFragmentationLibrary.dir/main.cpp.o"
+  "CMakeFiles/MessageFragmentationLibrary.dir/main.cpp.o.d"
+  "MessageFragmentationLibrary.exe"
+  "MessageFragmentationLibrary.pdb"
+  "libMessageFragmentationLibrary.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MessageFragmentationLibrary.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
